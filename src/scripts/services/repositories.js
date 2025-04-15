00c69899ea95d490d9d14e8e095eb6cecf0ae6db
Robert Castro <robertsmith86@hotmail.com>
2025-04-15 15:29:16 -0300

    caminho relativo

--- src/scripts/services/repositories.js
+++ src/scripts/services/repositories.js
@@ -1,12 +1,8 @@
-import { baseUrl } from '../variables.js';
+import { baseUrl, repositoriesQuantity } from '../variables.js';
 
-async function getUserRepositories(userName) {
-  const response = await fetch(`${baseUrl}/${userName}/repos?per_page=${repositoriesQuantity}`);
-  if (!response.ok) {
-    throw new Error(`HTTP error! status: ${response.status}`);
-  }
+async function getRepositories(userName) {
+  const response = await fetch(`${baseUrl}${userName}/repos?per_page=${repositoriesQuantity}`);
   return await response.json();
 }
 
-export { getUserRepositories };
-// Compare this snippet from src/scripts/services/user.js:
+export { getRepositories };
--- src/scripts/services/user.js
+++ src/scripts/services/user.js
@@ -2,9 +2,6 @@ import { baseUrl } from '../variables.js';
 
 async function getUser(userName) {
   const response = await fetch(`${baseUrl}${userName}`);
-  if (!response.ok) {
-    throw new Error(`HTTP error! status: ${response.status}`);
-  }
   return await response.json();
 }
 
